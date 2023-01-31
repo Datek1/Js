c43a5bcd320f864ce6dfdc477996f0fbd2519ce4
Datek1 <yurarudkevich81@gmail.com>
2023-01-31 22:19:03 +0200

    lesson 1

---
+++ lesson1/main.js
@@ -0,0 +1,30 @@
+let let1 = 'hello';
+let let2 = 'owu';
+let let3 = 'com';
+let let4 = 'ua';
+let let5 = 1;
+let let6 = 10;
+let let7 = -999;
+let let8 = 123;
+let let9 = 3.14;
+let let10 = 2.7;
+let let11 = 16;
+let let12 = true;
+let let13 = false;
+console.log(let1, let2, let3, let4, let5, let6, let7, let8, let9, let10, let11, let12, let13);
+
+let firstName = 'Yura';
+let midleName = 'Ivanovych';
+let lastName = 'Rudkevych';
+let person = `${firstName} ${midleName} ${lastName}`;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, typeof b, typeof c);
+
+let name = prompt('Enter your Name')
+let surName = prompt('Enter Surname')
+let age = prompt('Enter your age')
+console.log(name, surName, age);
