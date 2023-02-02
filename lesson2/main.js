5b3012a35b06a50df4a26500a6d5bd29c37613ad
Datek1 <yurarudkevich81@gmail.com>
2023-02-03 00:14:42 +0200

    upgrade lesson2

--- lesson2/main.js
+++ lesson2/main.js
@@ -141,11 +141,14 @@ if (num1 > num2) {
 // - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 // за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
 
-let m = undefined
-if (m = !!'fd' || !0 || !!undefined || !!null){
-    m = 'default'
-    console.log(m);
-}
+x = !!'' || 'default';
+console.log(x);
+x = !!0 || 'default'
+console.log(x);
+x = !!undefined || 'default'
+console.log(x);
+x = !!null || 'default'
+console.log(x);
 
 // з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 
