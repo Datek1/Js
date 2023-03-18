f2273f60204874d06c333caac12dd8e3ae49ad29
Datek1 <yurarudkevich81@gmail.com>
2023-03-19 00:22:49 +0200

    mini project

--- mini-project/index/users.js
+++ mini-project/index/users.js
@@ -5,16 +5,20 @@
 fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
+        const mainDiv = document.createElement('div')
+        mainDiv.classList.add('users')
         for (const user of users) {
-            const userUl = document.createElement('ul')
-            const userLi = document.createElement('li')
-            userLi.innerText = `${user.id} ${user.name}`
-            userUl.append(userLi)
+            const userDiv = document.createElement('div')
+            userDiv.classList.add('user')
+            const h3 = document.createElement('h3')
+            h3.innerText = `${user.id} ${user.name}`
             const aUser = document.createElement('a')
             aUser.innerText = 'link' + user.id
-            aUser.href = 'user-details.html?id='+JSON.stringify(user.id)
-            document.body.append(userUl, aUser)
+            aUser.href = '../user-details/user-details.html?id='+JSON.stringify(user.id)
+            userDiv.append(h3,aUser)
+            mainDiv.append(userDiv)
         }
+        document.body.append(mainDiv)
     })
 
 // при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів) отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
